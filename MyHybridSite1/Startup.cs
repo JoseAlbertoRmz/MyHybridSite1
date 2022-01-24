8f5ff63dd83804debc46f2563846c379eb8daabc
JOSE ALBERTO RAMIREZ PATIÑO <jose.ramirezp@my.uvm.edu.mx>
2022-01-24 15:45:06 -0600

    Agregar archivos de proyecto.

---
+++ MyHybridSite1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyHybridSite1.Startup))]
+namespace MyHybridSite1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
